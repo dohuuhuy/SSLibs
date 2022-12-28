96d2a0c48c3275ae46dcbf0b6da9ae8bc917bffd
dohuuhuy2 <dohuuhuy2@gmail.com>
2022-12-28 13:48:43 +0700

    useDataCalendar

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import SSCalendar from './components/SSCalendar'
+import useDataCalendar from './components/SSCalendar/HOCs/useDataCalendar'
 import SSCountDownUI from './components/SSCountDown'
 import useSSCountDown from './components/SSCountDown/HOCs'
 
-export { SSCalendar, SSCountDownUI, useSSCountDown }
+export { SSCalendar, SSCountDownUI, useSSCountDown, useDataCalendar }
