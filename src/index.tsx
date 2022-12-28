ba43e792b770d75b148ae5acb822f71d300fd0ed
dohuuhuy2 <dohuuhuy2@gmail.com>
2022-12-28 13:09:55 +0700

    .

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import SSCalendar from './components/SSCalendar'
 import SSCountDownUI from './components/SSCountDown'
 import useSSCountDown from './components/SSCountDown/HOCs'
 
-export default { SSCalendar, SSCountDownUI, useSSCountDown }
+export { SSCalendar, SSCountDownUI, useSSCountDown }
