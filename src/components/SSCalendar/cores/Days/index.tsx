b0c5bcaa94c2160e2571fe69fb425abfe141410a
dohuuhuy2 <dohuuhuy2@gmail.com>
2023-01-01 23:05:46 +0700

    .

--- src/components/SSCalendar/cores/Days/index.tsx
+++ src/components/SSCalendar/cores/Days/index.tsx
@@ -6,18 +6,20 @@ import styles from './styles.module.scss'
 export interface DaysProps {
   data: ItemDay[]
   onSelectDay?: (args: SelectDayArgs) => void
+  showLunar?: boolean
 }
 
-const Days: React.FC<DaysProps> = ({ data, onSelectDay }) => {
+const Days: React.FC<DaysProps> = ({ data, onSelectDay, showLunar = true }) => {
   return (
     <div className={styles.containerDays}>
       <ul className={styles.listCell}>
         {data.map((v, i = 0) => {
-
+          
           const passProps = {
             onSelectDay,
             value: v.days,
-            ...v
+            ...v,
+            showLunar
           }
 
           return (
